b3d35462d50a4678971abca08a5ad8dc0827d34d
andre custodio <andre.sc@outlook.pt>
2023-01-18 14:56:21 +0000

    ready to deploy

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "My Park",
+    subtitle: "An app to help you park around Porto, Portugal",
+    description:
+      "This app was developed with Ruby on Rails, ES6 Javascript and StimulusJS. It was a 2 week project with two colleagues from LeWagon. Huge shoutout to them, you know who you are!",
+    image: "./mypark1.gif",
+    link: "https://my-park-zda.herokuapp.com/",
+  },
+  {
+    title: "My Park",
+    subtitle: "An app to help you park around Porto, Portugal",
+    description:
+      "This app was developed with Ruby on Rails, ES6 Javascript and StimulusJS. It was a 2 week project with two colleagues from LeWagon. Huge shoutout to them, you know who you are!",
+    image: "./mypark2.gif",
+    link: "https://my-park-zda.herokuapp.com/",
+  }
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Ruby on Rails",
+  "HTML/CSS/SCSS",
+  "Git/Github",
+  "<render LearnNewSkill /> "
+];
