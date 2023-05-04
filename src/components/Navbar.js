3003c5dbc01febfde697773d588520b13dc5028a
andre custodio <andre.sc@outlook.pt>
2023-05-04 11:49:53 +0100

    changed github link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="https://www.github.com/andresc89" className="mr-5 hover:text-white">
+          <a href="https://www.github.com/ascustodiowebdev" className="mr-5 hover:text-white">
             My github
           </a>
           <a href="#skills" className="mr-5 hover:text-white">
